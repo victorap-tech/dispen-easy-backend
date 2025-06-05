cc4700e52a0603070d25e36e80ca431f70b36215
victorap-tech <victorapostoles@gmail.com>
2025-06-05 19:13:03 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Endpoint de prueba
 @app.route('/')
 def home():
-    return 'Servidor Dispen-easy activo'
+    return 'Backend de Dispen-Easy funcionando correctamente.'
+
+# Endpoint para verificar pago
+@app.route('/check_payment', methods=['GET'])
+def check_payment():
+    id_pago = request.args.get('id_pago')
+    if id_pago == '312344':
+        return jsonify({"estado": "aprobado", "status": "ok"})
+    else:
+        return jsonify({"message": "Pago no encontrado", "status": "error"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
