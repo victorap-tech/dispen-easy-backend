0075161fe3e9e9aea252aba56a511431ffaba67f
victorap-tech <victorapostoles@gmail.com>
2025-06-11 21:29:43 -0300

    app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def check_payment():
 import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environt.get('PORT',5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
