a149a6e6514e885e4edc7be984263461cc812c12
victorap-tech <victorapostoles@gmail.com>
2025-07-17 13:47:55 -0300

    app.py

--- app.py
+++ app.py
@@ -84,9 +84,9 @@ def marcar_dispensado():
     return jsonify({'status': 'ok'}), 200
 
 # Ejecutar servidor Flask
-if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=8000)
+#if __name__ == '__main__':
+    #init_db()
+    #app.run(host='0.0.0.0', port=8000)
 
 
 
