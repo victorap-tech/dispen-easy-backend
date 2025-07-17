b34ef9a08150cc2f4bf740dbf9400a14d428223f
victorap-tech <victorapostoles@gmail.com>
2025-07-17 13:39:14 -0300

    app.py

--- app.py
+++ app.py
@@ -86,3 +86,7 @@ def marcar_dispensado():
 @app.route('/')
 def home():
     return 'Servidor Dispen-Easy funcionando'
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=8000)
