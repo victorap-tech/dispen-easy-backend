d4f47c1007d787ffff69c6d6968fd5f10cc7a68f
victorap-tech <victorapostoles@gmail.com>
2025-07-23 09:10:12 -0300

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app)
 
 DB_PATH = 'productos.db'
 
-ACCESS_TOKEN = "TU_ACCESS_TOKEN_AQUI"  # ← Cambia esto por tu token de MercadoPago
+ACCESS_TOKEN = "APP_USR-7903926381447246-061121-b38fe6b7c7d58e0b3927c08d041e9bd9-246749043"  # ← Cambia esto por tu token de MercadoPago
 
 # -------- INICIALIZAR TABLAS --------
 @app.route('/initdb')
