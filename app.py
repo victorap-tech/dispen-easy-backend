ef90de0cc8f0aff52f6d9f1a3b9e3f596658ad10
victorap-tech <victorapostoles@gmail.com>
2025-07-30 15:23:21 -0300

    app.py

--- app.py
+++ app.py
@@ -125,6 +125,6 @@ def marcar_dispensado():
     return jsonify({"status": "no_encontrado"})
 
 if __name__ == '__main__':
-    with app.app_context()
+    with app.app_context():
     db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
